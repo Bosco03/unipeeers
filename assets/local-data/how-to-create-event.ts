abfc6529af6ff1f0089e0997b4d4ed16979253d1
Bosco <johnboscoinnocent09@gmail.com>
2025-08-19 11:09:15 -0800

    creat tab

---
+++ assets/local-data/how-to-create-event.ts
@@ -0,0 +1,22 @@
+export const howToCreateEvent = [
+    {
+        title: "Title",
+        doc: "Type the name of your event"
+    },
+    {
+        title: "Price",
+        doc: "You can choose if your event is Free or Paid."
+    },
+    {
+        title: "Venue",
+        doc: "Enter the location"
+    },
+    {
+        title: "Time",
+        doc: "Pick the date and time for your event."
+    },
+    {
+        title: "Description",
+        doc: "Write details about your event"
+    },
+];
