720321ae3bba2025f8d8bbe56b879ddfce93a65c
Bosco <johnboscoinnocent09@gmail.com>
2025-08-19 16:01:49 -0800

    datebutton

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,12 +1,21 @@
 import { howToCreateEvent } from "@/assets/local-data/how-to-create-event";
+import { formatTimestampToDate } from "@/utils/format-date.utils";
 import { themeColors } from "@/utils/theme.utils";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { useState } from "react";
-import { Platform, ScrollView, StatusBar, StyleSheet, Text, TextInput, View } from "react-native";
+import { Platform, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default  function Create () {
     const [title,setTitle] = useState("")
+    const [description,setDescription] = useState("")
+    const [date,setDate] = useState(new Date());// console.log(title);
+    const [showPicker,setShowPicker] = useState(false)
 
-    console.log(title);
+    const onChange = (event,selectedDate) => {
+        const currentDate = selectedDate || date;
+        setDate(currentDate);
+        setShowPicker(false);
+    }
 
     return(
       <View style={styles.main}>
@@ -16,13 +25,48 @@ export default  function Create () {
             {/* event creation form */}
             <View className="flex gap-4 bg-white rounded-md p-3" >
                 <View>
-                  <Text className="text-xs text-neutral-500">event title</Text>
+                  <Text className="text-md text-neutral-500">event title</Text>
                   <TextInput
                   style={styles.input}
+                  value={title}
                   placeholder="title of your event"
                   onChangeText={(text) => setTitle(text)}/>
                 </View>
+
+                <View>
+                  <Text className="text-md text-neutral-500">event description</Text>
+                  <TextInput
+                  multiline={true}
+                  style={styles.textarea}
+                  value={description}
+                  placeholder="title of your event"
+                  onChangeText={(text) => setDescription(text)}/>
+                </View>
+
+                <View>
+              <TouchableOpacity
+              onPress={() => setShowPicker(true)}
+              style={{
+                backgroundColor: themeColors.darkGray,
+                borderRadius: 50,
+                paddingHorizontal: 16,
+                paddingVertical:8,
+              }}
+              className="flex flex-row justify-between items-center">
+                <Text className="font-bold text-lg text-neutral-100">{formatTimestampToDate(date)}</Text>
+                <Text className="font-bold text-2xl text-neutral-100">select event date</Text>
+              </TouchableOpacity>
+              {showPicker && (
+                    <DateTimePicker
+                    testID="dateTimePicker"
+                    model="date"
+                    value={date}
+                    onChange={onChange}/>
+              )}
             </View>
+            </View>
+
+            
 
             {/* how to create event documentation */}
             <View className="flex gap-4 bg-white rounded-md p-3">
@@ -42,16 +86,23 @@ const styles = StyleSheet.create({
     main:{
         flex:1,
         gap: 16,
-        backgroundColor: themeColors.lightGreen, 
         paddingTop: Platform.OS === "ios" ? 24 : StatusBar.currentHeight,
         paddingHorizontal: 12,
     },
     input:{
-      borderWidth: 2,
-      borderColor: themeColors.lightGreen,
+      borderWidth: 1,
+      borderColor: themeColors.darkGray,
       paddingHorizontal: 16,
       paddingVertical: 8,
       borderRadius: 50,    
       fontSize: 16,
-    }
+    },
+    textarea:{
+      borderWidth: 1,
+      borderColor: themeColors.darkGray,
+      paddingHorizontal: 16,
+      paddingVertical: 8,
+      borderRadius: 8,    
+      fontSize: 16,
+    },
 })
