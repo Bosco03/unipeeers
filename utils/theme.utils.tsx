5aef7352596d135c8ba2ca5775c887251e4df213
Bosco <johnboscoinnocent09@gmail.com>
2025-08-28 11:10:49 -0800

    react native select picker fix

--- utils/theme.utils.tsx
+++ utils/theme.utils.tsx
@@ -1,6 +1,9 @@
 export const themeColors = {
-    black:"#000000",
-    darkGray:"#222222",
-    lightGreen:"#1DCD9F",
-    darkGreen:"#169976"
+    black: "#000000",
+    gray100: "#EEEEEE",
+    gray200: "#E9E9E9",
+    gray300: "DDDDDD",
+    darkGray: "#222222",
+    lightGreen: "#1DCD9F",
+    darkGreen: "#169976"
 }
