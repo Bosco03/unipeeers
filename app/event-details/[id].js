924bc868f98bbc88a054a2386c721caf6f103095
Bosco <johnboscoinnocent09@gmail.com>
2025-08-28 11:43:51 -0800

    events on home ui redesign

--- app/event-details/[id].js
+++ app/event-details/[id].js
@@ -1,93 +1,101 @@
 import { events } from "@/assets/local-data/events";
-import { EventSnippet } from "@/components/EventSnippet";
 import { themeColors } from "@/utils/theme.utils";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
 import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { EventSnippet } from "../../components/EventSnippet";
 
-const screenwidth = Dimensions.get("window").width*0.94;
+const screenWidth = Dimensions.get("window").width * 0.94;
 
-export default  function EventDatails () {
-    const {id} = useLocalSearchParams();  // console.log(">>>>>",id);
+export default function EventDetails () {
+    const { id } = useLocalSearchParams();
     const [data,setData] = React.useState(undefined);
-    
+
     React.useEffect(() => {
-      if (id !== undefined && id !== null && id !== "") {
-        const filteredData = events.filter(item => item.id === id)[0];
-        setData(filteredData);
-    }
+        if (id !== undefined && id !== null && id !== "") {
+            const filteredData = events.filter(item => item.id === id)[0];
+            setData(filteredData);
+        }
     },[id]);
 
-    // convert event timestamp to data
+    // convert event timestamp to date
     const convertTimestamp = (stamp) => {
-      const eventDate = new Date(stamp);
-      return String(eventDate);
+        const eventDate = new Date(stamp);
+        return String(eventDate);
     }
 
-    const decidedFee = (free,fee) => {
-      let feeText = ""
-      if (free === true) {
-        feeText = "FREE"
-      }else{
-        feeText = `₦${fee}`
-      }
+    const decideFee = (free,fee) => {
+        let feeText = "";
+        if (free === true) {
+            feeText = "FREE"
+        } else {
+            feeText = `₦${fee}`
+        }
 
-      return feeText;
-    }
+        return feeText;
+    } 
 
-    if (data !== undefined){
-      return(
-      <SafeAreaProvider >
-        <SafeAreaView style={{flex: 1,paddingVertical:16, display: "flex", justifyContent: "space-between"}}>
-          {/* upper group */}
-          <View className="flex gap-y-3"> 
-            <View className="flex flex-row justify-center">
-              <Image
-              source={{uri: data.bannerUrl}}
-              style={{width: screenwidth,height:280,resizeMode:"cover"}}
-              className="rounded-md"
-              alt="event cover image"/>
-            </View>
+    if (data !== undefined) {
+        return (
+            <SafeAreaProvider>
+                <SafeAreaView style={{ flex:1,paddingVertical:16,display:"flex",justifyContent:"space-between" }}>
+                    {/* upper group */}
+                    <View className="flex gap-y-3">
+                        <View className="flex flex-row justify-center">
+                            <Image
+                            source={{ uri: data.bannerUrl}}
+                            style={{width: screenWidth,height:280,resizeMode:"cover"}}
+                            className="rounded-md"
+                            alt="event cover image"/>
+                        </View>
 
-            {/* body area */}
-            <View className="flex justify-between gap-y-2 px-3">
-              <EventSnippet 
-              mainTitle={data.time} 
-              subTitle={data.date}
-              iconName="event-note"/>
+                        {/* body area */}
+                        <View className="flex justify-between gap-y-2 px-3">
+                            <EventSnippet 
+                            mainTitle={data.time} 
+                            subTitle={data.date} 
+                            iconName="newspaper"/>
 
-              <EventSnippet 
-              mainTitle={data.title} 
-              subTitle={data.createdBy}
-              iconName="group"/>
+                            <EventSnippet 
+                            mainTitle={data.title} 
+                            subTitle={data.createdBy}
+                            iconName="event-note"/>
 
-              <EventSnippet 
-              mainTitle={decidedFee(data.free,data.fee)} 
-              subTitle={convertTimestamp(data.createdAt)}
-              iconName="account-balance-wallet"/>
+                            <EventSnippet 
+                            mainTitle={decideFee(data.free,data.fee)} 
+                            subTitle={convertTimestamp(data.createdAt)}
+                            iconName="account-balance-wallet"/>
 
-              <EventSnippet 
-              mainTitle={data.venue} 
-              subTitle={data.school}
-              iconName="location-city"/>
+                            <EventSnippet 
+                            mainTitle={data.venue} 
+                            subTitle={data.school}
+                            iconName="map"/>
 
-              {/* description block */}
-              <View>
-                <Text style={{color: themeColors.darkGreen, fontWeight: "bold"}}>Event Description</Text>
-                <Text>{data.desc}</Text>
-              </View>
-            </View>
-          </View>
+                            {/* description block */}
+                            <View>
+                                <Text style={{color: themeColors.darkGreen, fontWeight: "bold"}}>Event description</Text>
+                                <Text>{data.desc}</Text>
+                            </View>
+                        </View>
+                    </View>
 
-          {/* bottom group */}
-          <View className="px-3">
-            <TouchableOpacity style={{backgroundColor: themeColors.darkGreen}} className="h-16 flex justify-center items-center rounded-full">
-              <Text className="text-3xl text-white">Save event</Text>
-            </TouchableOpacity>
-          </View>
-        </SafeAreaView>
-      </SafeAreaProvider>
-    )
+                    {/* bottom group */}
+                    <View className="px-3">
+                        <TouchableOpacity style={{backgroundColor: themeColors.darkGreen}} className="h-16 flex justify-center items-center rounded-full">
+                            <Text className="text-3xl text-white">Save event</Text>
+                        </TouchableOpacity>
+                    </View>
+                </SafeAreaView>
+            </SafeAreaProvider>
+        )
+    } else {
+        return (
+            <SafeAreaProvider>
+                <SafeAreaView style={{ flex:1,paddingVertical:16,display:"flex",justifyContent:"space-between" }}>
+                    <Text style={{ fontSize: 16 }}>Undefined event</Text>
+                </SafeAreaView>
+            </SafeAreaProvider>
+        )
     }
 }
