9e7a088fcd2acf7c2c20fd272b4005362e97bd1b
Bosco <johnboscoinnocent09@gmail.com>
2025-08-28 15:51:30 -0800

    index/tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,66 +1,48 @@
 import { events } from "@/assets/local-data/events";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link } from "expo-router";
-import { Dimensions, FlatList, Image, Text, View } from "react-native";
+import { EventSnippet } from "@/components/EventSnippet";
+import { useFonts } from "expo-font";
+import { FlatList, StyleSheet, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-// calculate screen width
-const screenWidth = Dimensions.get("window").width;
-// console.log(screenWidth)
 
-function seperator () {
+function Seperator () {
   return (
     <View style={{height:32, backgroundColor:"transparent"}}></View>
   )
 }
 
-export default  function Index () {
-  return(
-   <SafeAreaProvider>
-    <SafeAreaView style={{
-      flex: 1
-    }}>
-      <FlatList
-      data={events}
-      ItemSeparatorComponent={seperator}
-      renderItem={({item}) => {
-        return (
-          <View className="relative">
-            <Image
-            source={{uri: item.bannerUrl}}
-            alt="event photo"
-            style={{width:screenWidth,height:400, resizeMode:"cover"}}/>
+export default function Index () {
+  const [fontsLoaded] = useFonts({
+    "Bodoni-Bold": require("../../assets/fonts/BodoniFLF-Bold.ttf"),
+    "Raleway-Bold": require("../../assets/fonts/Raleway-Bold.ttf"),
+    "Raleway-Regular": require("../../assets/fonts/Raleway-Regular.ttf")
+  });
 
-            {/* this view is set to absolute on event components */}
-            <View className="absolute top-3 left-3 p-3 bg-teal-800 rounded-md">
-              {item.free === true 
-              ?
-              <Text className="text-white font-bold">Free</Text>
-              :
-              <Text className="text-white font-bold">₦{item.fee}</Text>
-              }
-            </View>
-            
-            <View className="px-3">
-              <View className="p-3 rounded-b-lg border border-teal-600 border-t-0">
-                <Text className="text-xs font-bold">{item.title}</Text>
-                <View className="flex flex-row justify-between">
-                  <View className="flex flex-row items-center gap-x-2">
-                    <FontAwesome 
-                    name="user-circle" 
-                    size={24} 
-                    color="black" />
-                    <Text>{item.createdBy.length > 24 ? `${item.createdBy.slice(0,24)}....` : item.createdBy}</Text>
-                  </View>
-                  <Link href={`/event-details/${item.id}`} className="p-3 font-bold text-white bg-teal-800 rounded-md">
-                    <Text>Event Details</Text>
-                  </Link>
-                </View>
-              </View>
-            </View>
-          </View>
-        )
-      }}/>
-    </SafeAreaView>
-   </SafeAreaProvider>
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <FlatList
+        data={events}
+        ItemSeparatorComponent={Seperator}
+        renderItem={({item}) => {
+          return (
+            <EventSnippet data={item}/>
+          )
+        }}
+        keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    fontFamily: "Bodoni-Bold",
+    fontWeight: "bold"
+  }
+})
